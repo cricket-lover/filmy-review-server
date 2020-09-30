cf87d5ba213a78b98f23e26898dd886cb08052dd
phaneendra <phaneendraandukuri@gmail.com>
2020-09-30 10:34:56 +0530

    added movie data in movieDetails.js

---
+++ movieDetails.js
@@ -0,0 +1,108 @@
+exports.movieDetails = [
+  {
+    id: 1,
+    image: '/images/bahubali3.jpg',
+    title: 'Bahubali',
+    dateOfRelease: 'July 10, 2015',
+    reviews: [
+      {
+        headline: 'A new bench mark for Indian Cinema!!!',
+        content:
+          'Im a big fan of Rajamouli, but this is not a biased review. The rating I gave is not just for Rajamouli but the entire unit of Baahubali. Right from the story which reminded me of all the beautiful Indian stories that were part of my childhood,(Im getting back to reading Amar Chitra Katha now!!!) to every guy who says "Baahubali" in the movie. There were several scenes that gave me goosebumps, not scary ones but like those that put you in awe.',
+      },
+      {
+        headline: 'A superb visual treat.',
+        content:
+          'If a person is bit of into movies, I am sure that person heard about Baahubali couple of years back. It took almost four years to complete the first part. Hero Prabhas had completely dedicated his time to this movie and no movie after his super successful movie Mirchi. Same is true from S.S Rajamouly, after his last non-conventional movie Eega. So obviously there are high expectations.',
+      },
+      {
+        headline: 'Rajamouli leaves his mark.. conclusion awaits',
+        content:
+          'Visuals are good. All characters did their part. Cannot name anyone particularly. I liked the main character most. I will be watching the movie at least two more times :). Hope you all enjoy it.',
+      },
+    ],
+  },
+  {
+    id: 2,
+    image: '/images/msdhoni.jpg',
+    title: 'M.S.Dhoni',
+    dateOfRelease: 'September 30, 2016',
+    reviews: [],
+  },
+  {
+    id: 3,
+    image: '/images/avengers.jpg',
+    title: 'Avengers',
+    dateOfRelease: 'April 26, 2019',
+    reviews: [
+      {
+        headline: 'The ending made all 22 movies worth it',
+        content:
+          'The crowd applauded 3 times during the movie, and stood up to clap their hands after. This I have never witnessed in a Dutch cinema. Dutch crowds are not usually passionate about this. ',
+      },
+    ],
+  },
+  {
+    id: 4,
+    image: '/images/inception.jpg',
+    title: 'Inception',
+    dateOfRelease: 'July 16, 2010',
+    reviews: [
+      { headline: 'Awesome movie', content: 'A goddamn well acted cast' },
+      {
+        headline: 'Excellent movie',
+        content: 'Good perfomance by all the actors',
+      },
+      { headline: 'Great movie', content: 'Nicely done by the team' },
+    ],
+  },
+  {
+    id: 5,
+    image: '/images/munthiriMonchan.jpg',
+    title: 'Munthiri Monchan',
+    dateOfRelease: 'December 06, 2019',
+    reviews: [],
+  },
+  {
+    id: 6,
+    image: '/images/jersey.jpg',
+    title: 'Jersey',
+    dateOfRelease: 'April 26, 2019',
+    reviews: [],
+  },
+  {
+    id: 7,
+    image: '/images/kgf2.jpeg',
+    title: 'K.G.F 2',
+    dateOfRelease: 'October 23, 2020',
+    reviews: [
+      { headline: 'Awesome movie', content: 'A goddamn well acted cast' },
+      {
+        headline: 'Excellent movie',
+        content: 'Good perfomance by all the actors',
+      },
+      { headline: 'Great movie', content: 'Nicely done by the team' },
+    ],
+  },
+  {
+    id: 8,
+    image: '/images/vikramVedha.jpg',
+    title: 'Vikram Vedha',
+    dateOfRelease: 'July 21, 2017',
+    reviews: [],
+  },
+  {
+    id: 9,
+    image: '/images/sairat.jpg',
+    title: 'Sairat',
+    dateOfRelease: 'April 29, 2016',
+    reviews: [],
+  },
+  {
+    id: 10,
+    image: '/images/premam.jpg',
+    title: 'Premam',
+    dateOfRelease: 'April 29, 2016',
+    reviews: [],
+  },
+];
